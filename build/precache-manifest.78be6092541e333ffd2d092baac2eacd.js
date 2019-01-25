6dafcebedc0d5f37b89d08e3335456732fda1ccc
Lillia Jackson <lillilj9@gmail.com>
2019-01-25 16:56:08 -0700

    config var

---
+++ build/precache-manifest.78be6092541e333ffd2d092baac2eacd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "64e09b0591d56e0e63d5",
+    "url": "/alpha3/static/css/main.e37642a3.chunk.css"
+  },
+  {
+    "revision": "64e09b0591d56e0e63d5",
+    "url": "/alpha3/static/js/main.64e09b05.chunk.js"
+  },
+  {
+    "revision": "09412f3242007d97d1a0",
+    "url": "/alpha3/static/css/1.7d37fe55.chunk.css"
+  },
+  {
+    "revision": "09412f3242007d97d1a0",
+    "url": "/alpha3/static/js/1.09412f32.chunk.js"
+  },
+  {
+    "revision": "b3c79406dbc8ee355cd9",
+    "url": "/alpha3/static/js/runtime~main.b3c79406.js"
+  },
+  {
+    "revision": "238c687fcd5154de79c6c83c0bbed2b5",
+    "url": "/alpha3/static/media/alpha-logo.238c687f.png"
+  },
+  {
+    "revision": "e93d0e425688bab378f2d7bf348e0697",
+    "url": "/alpha3/static/media/alpha-nav.e93d0e42.png"
+  },
+  {
+    "revision": "3a114c0b4aeb2d7707f015bb2c6c5f72",
+    "url": "/alpha3/static/media/search.3a114c0b.png"
+  },
+  {
+    "revision": "504e1674bb51a5b77884e9845c1ff288",
+    "url": "/alpha3/static/media/chat.504e1674.png"
+  },
+  {
+    "revision": "558b80423652d849c185d6de336fd2a0",
+    "url": "/alpha3/index.html"
+  }
+];
--- src/config.js
+++
@@ -1,9 +0,0 @@
-// Chatkit Auth Tokens
-
-const tokenUrl = "YOUR_URL";
-
-const instanceLocator = "YOUR_INSTANCE";
-
-// const chatKey = "";
-
-export {instanceLocator, tokenUrl}
